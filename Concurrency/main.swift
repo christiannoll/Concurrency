081045680064c18a0c6491605ffa146d6b209d9b
Christian Noll <christiannoll@gmx.net>
2021-10-26 21:03:24 +0200

    test async/await in swift

--- Concurrency/main.swift
+++ Concurrency/main.swift
@@ -1,11 +1,37 @@
-//
-//  main.swift
-//  Concurrency
-//
-//  Created by Christian on 01.10.21.
-//
-
 import Foundation
 
-print("Hello, World!")
+
+func performTaskA() async -> Int {
+    print("Start A")
+    // Wait for 10 seconds
+    await Task.sleep(10 * 1_000_000_000)
+    print("End A")
+    return 3
+}
+
+func performTaskB() async -> Int {
+    print("Start B")
+    await Task.sleep(11 * 1_000_000_000)
+    print("End B")
+    return 2
+}
+
+let runLoop = CFRunLoopGetCurrent()
+
+func doSomething() {
+    Task(priority: .high) {
+        // Create & start a child task
+        async let a = performTaskA()
+
+        // Create & start a child task
+        async let b = performTaskB()
+
+        let sum = await (a + b)
+        print(sum)
+        CFRunLoopStop(runLoop)
+    }
+}
+
+doSomething()
+CFRunLoopRun()
 
